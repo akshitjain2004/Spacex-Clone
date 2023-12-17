7600cb6e54645556b86397ebde49626b8df0ce5a
Akshit Jain <akshitjain38630@gmail.com>
2023-12-17 15:35:04 +0530

    video fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import videoBg from "../assets/x2mate.com-NASA's Mars 2020 Perseverance Rover Landing Animations-(1080p60).mp4"
+import "./Home.css"
 const Home = () => {
   return (
 <section>
-  <div className="home-bg">
-    <video src={videoBg} autoPlay loop muted></video>
+  <div className="video-container">
+    <video src={videoBg} autoPlay loop muted  width="1280" height="720"></video>
   </div>
         <div className="home-content">
     <h3>RECENT LAUNCH</h3>
